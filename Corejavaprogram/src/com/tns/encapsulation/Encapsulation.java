698b52c82f29d23a3e7b64f81744fb470c29beb4
ADMIN <ADMIN@DESKTOP-U8MCOL1>
2024-06-16 16:31:57 +0530

    commit message

---
+++ Corejavaprogram/src/com/tns/encapsulation/Encapsulation.java
@@ -0,0 +1,12 @@
+package com.tns.encapsulation;
+// demo encapsulation
+public class Encapsulation {
+	     int serialnumber; // properties or data members
+	     String name;
+	     int age;
+	     
+	     public void show() {   // method or member function
+	    	     System.out.println("serialnumber "+serialnumber+" "+"+name+ "+name+" "+"age  "+age);
+	     }
+
+}
