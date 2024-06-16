2b23b4db0dc7e4fee05de41e0b5360ace6a9824a
ADMIN <ADMIN@DESKTOP-U8MCOL1>
2024-06-16 14:56:13 +0530

    commit message

---
+++ Corejavaprogram/src/com/tns/packagesprogram/Publicexample.java
@@ -0,0 +1,15 @@
+package com.tns.packagesprogram;
+// demo packages
+public class Publicexample {
+
+	int a=9;
+	       protected char f='y';  // data members or properties
+	       public static void main(String[] args) {
+		          Publicexample u=new Publicexample();// object creation
+		          
+		          System.out.println(u.a);
+		          System.out.println(u.f);
+
+	}
+
+}
