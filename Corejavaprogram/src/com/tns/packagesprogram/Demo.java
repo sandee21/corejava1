680c118195759d6595b4d51c0236dfa1c68a5f96
ADMIN <ADMIN@DESKTOP-U8MCOL1>
2024-06-16 15:23:08 +0530

    commit message

---
+++ Corejavaprogram/src/com/tns/packagesprogram/Demo.java
@@ -0,0 +1,11 @@
+package com.tns.packagesprogram;
+
+public class Demo {
+        public static void main(String[] args) {
+		       Publicexample u=new Publicexample();
+		System.out.println(u.a);
+		       System.out.println(u.f);
+
+	}
+
+}
